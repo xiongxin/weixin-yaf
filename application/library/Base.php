8cdfef1c40122de5dfb7ae0c3fc8002013f81aa2
熊鑫 <1781189926@qq.com>
2016-03-29 16:30:42 +0800

    修改

--- application/library/Base.php
+++ application/library/Base.php
@@ -24,18 +24,16 @@ class Base extends Controller_Abstract {
             'appid'=>'wx1a08bb8a9459ad7e', //填写高级调用功能的app id, 请在微信开发模式后台查询
             'appsecret'=>'7f0689ead41f57df91109a7054127794', //填写高级调用功能的密钥
             'token_ctime' => 12222,
-            'access_token'=> 'aaa',
-            'expires_in'=> 122
+            "access_token" => "Ov-I9QRBcvgJRzMc31RGxcE_4QBAvXUWeMK3NUK4Z4FaF_TDUgUi8T5PmMXL8TkI4naUz4EXhCp1cD44rX7SQwf_-pht18Eqtf_hFSSvGS8-sjnp_cmsn-cgd71GGjnsPHXdAAATLB",
+            "expires_in" => 7200
         );
         $this->wechat = new Wechat($options);
-        var_dump($_SERVER);
         if(!is_not_wx() && empty($this->user)) {
-            $forward = urlencode('http://' . $_SERVER['HTTP_HOST'] . $this->getRequest()->getRequestUri());
-            $url = DOMAIN.'/callback/spread?forward='.$forward;
-
+            $forward = urlencode(DOMAIN.$_SERVER['REQUEST_URI']);
+            $url = DOMAIN.'/callback/spread.html?forward='.$forward;
+            $this->redirect($this->wechat->getOauthRedirect($url,'STATE','snsapi_userinfo'));
             //跳转回调接口
             //$url回调地址
-            $this->redirect($this->wechat->getOauthRedirect($url,'','snsapi_base'));
         }
 
         $this->getView()->getTwig()->addGlobal('user', $this->user); //添加一个全局变量
--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
 
 require_once '../vendor/autoload.php';
 
+define('DOMAIN','http://'.$_SERVER['HTTP_HOST']);
 define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
 $app  = new Yaf\Application(APP_PATH . "/conf/application.ini");
 $app->bootstrap()->run();
